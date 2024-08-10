e50ed7883c61e74a40b27a8358add9997d39796e
HARUDe <19623194@std.stei.itb.ac.id>
2024-08-10 23:29:50 +0700

    feat: Portfolio Website

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,159 @@
+// Import required modules
 import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+// Home component
+const Home = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="bg-blue-900" style={{ cursor: "url(/cursorblues.png), auto"}}>
+      <div className="flex flex-col">
+        <div className="bg-gradient-to-r from-blue-600 to-blue-900 h-16 flex items-center">
+          <Image
+            src={"/totoro_nobg.png"}
+            alt="Foto Profil"
+            width={50}
+            height={50}
+            className="rounded-full ml-4"
+          />
+          <div className="p-4">
+            <ul className="text-white flex justify-end">
+              <li className="shadow-lg" style={{ margin: "0 1rem", fontWeight: "bold", backgroundColor: "bg-blue-900" }}>
+                <Link href="#profile">Profile</Link>
+              </li>
+              <li className="shadow-lg" style={{ margin: "0 1rem", fontWeight: "bold", backgroundColor: "bg-blue-900" }}>
+                <Link href="#hobby">Skill</Link>
+              </li>
+              <li className="shadow-lg" style={{ margin: "0 1rem", fontWeight: "bold", backgroundColor: "bg-blue-900" }}>
+                <Link href="#portfolio">Portfolio</Link>
+              </li>
+              <li className="shadow-lg" style={{ margin: "0 1rem", fontWeight: "bold", backgroundColor: "bg-blue-900" }}>
+                <Link href="#contact">Contact</Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-white text-6xl font-bold">Welcome to Hazim Portfolio</h1>
+        <p className="text-white text-lg">Scroll down to learn more about me!</p>
       </div>
     </main>
   );
-}
+};
+
+// Profile component
+const Profile = () => {
+  return (
+    <div className="flex flex-col bg-blue-900 h-screen" style={{ cursor: "url(/cursorblues.png), auto"}}>
+      <div className="flex flex-row">
+        <Image
+          src={"/fotoportofoliofix.jpg"}
+          alt="Foto Profil"
+          width={400}
+          height={400}
+          className="rounded-full mt-4 ml-10"
+        />
+        <h2 className="headersize headersize:hover ml-5" style={{ color: "white", fontWeight: "bold", fontFamily: "sans-serif", fontStyle: "italic" }}>
+          My Profile
+        </h2>
+      </div>
+      <p className="mt-4 ml-40 " style={{ color: "white", fontFamily: "sans-serif", fontStyle: "italic" }}>
+        Hello, my name is Muhammad Hazim Ramadhan Prajoda and I'm a college student at Institut Teknologi Bandung. 
+        I'm currently studying Informatics Engineering and I'm in my 2nd year as of now. While in High School I still don't know where to continue my study,
+        I choose ITB because it seems like a good place for me to learn and grow. Although I choose this major, I'm quite struggling with a lot of coding and programming, 
+        but I'm still trying to learn and improve myself.
+      </p>
+      <p className="mt-4 ml-40 " style={{ color: "white", fontFamily: "sans-serif", fontStyle: "italic" }}>
+        Right now I'm interested in learning more about web development and programming, I'm still learning and trying to improve my skills in this field.
+        I'm also interested in Cybersecurity but I'm still at the starting point right now. 
+      </p>
+      <p className="mt-4 ml-40 " style={{ color: "white", fontFamily: "sans-serif", fontStyle: "italic" }}>
+        One of my achievement is getting to ITB through the SNBT test, I'm quite proud of it because I'm the first person in my family to get into ITB.
+      </p>
+    </div>
+  );
+};
+
+
+const Skill = () => {
+  return (
+    <div className="bg-blue-900 justify-center" style={{ cursor: "url(/cursorblues.png), auto"}}>
+      <h1 className="text-white text-4xl font-bold mb-4" style={{ fontFamily: "sans-serif", textAlign: "center" }}>My Talents and Skills</h1>
+      <div className="flex flex-row justify-center">
+        <div className="talent-item marginlarge">
+          <h2 style={{ fontFamily: "sans-serif", fontWeight: "bold" }}>Mathematics</h2>
+          <img src="/mathimg.jpg" alt="Mathematics" className="justify-center" style={{ width: "100px", height: "150px" }} />
+          <p>My math skills is on a mid to high level for 1st year college student, I'm quite confident with it although not to the point 
+            where I can say I'm a math genius
+          </p>
+        </div>
+        <div className="talent-item marginlarge">
+          <h2 style={{ fontFamily: "sans-serif", fontWeight: "bold" }}>Memorizing</h2>
+          <img src="/memorizing.png" alt="Memorizing" style={{ width: "100px", height: "150px" }} />
+          <p>I can memorize something fast if I deem it something important enough.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+// Portfolio component
+const Portfolio = () => {
+  return (
+    <div className="bg-blue-900 pt-40 justify-center" style={{ cursor: "url(/cursorblues.png), auto"}}>
+      <h1 className="text-white text-4xl font-bold mb-4" style={{ fontFamily: "sans-serif", textAlign: "center" }}>My Portfolio</h1>
+      <p className="text-white pl-10">Check out my project:</p>
+      <ul className="text-white flex justify-center">
+        <li className="marginlarge">
+          <img src="/project1.jpg" alt="Project 1" className="items-center" style={{ width: "200px", height: "200px" }} />
+          <p>One of my team "Weapon Creation" project for the GST Fest 23, it was making some props for cosplay.</p>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+// Contact component
+const Contact = () => {
+  return (
+    <div className="bg-blue-900 pt-40 justify-center" style={{ cursor: "url(/cursorblues.png), auto"}}>
+      <h1 className="text-white text-4xl font-bold mb-4" style={{ fontFamily: "sans-serif", textAlign: "center" }}>Contact Me</h1>
+      <p className="text-white pl-10 ">You can reach out to me through the following channels:</p>
+      <ul className="text-white flex justify-center">
+        <li style={{ fontWeight: "bold", display: "flex", flexDirection: "row" }}>
+          Media: 
+          <a href="https://www.linkedin.com/in/muhammadhazimrp" target="_blank" rel="noopener noreferrer">
+            <img src="/linkedinlogo.png" alt="Contact Image" className="contactlogo mt-4" />
+          </a>
+          <a href="https://github.com/HARUDe" target="_blank" rel="noopener noreferrer">
+            <img src="/githublogo.png" alt="Contact Image" className="contactlogo contactlogo:hover mt-4" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+// Page component
+const Page = () => {
+  return (
+    <>
+      <Home />
+      <section id="profile">
+        <Profile />
+      </section>
+      <section id="hobby">
+        <Skill />
+      </section>
+      <section id="portfolio">
+        <Portfolio />
+      </section>
+      <section id="contact">
+        <Contact />
+      </section>
+    </>
+  );
+};
+
+export default Page;
